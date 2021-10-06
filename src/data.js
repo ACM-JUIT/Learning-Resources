b5586f96f33e47294566b47eb4e270bab043c287
Abhinandan Thakur <73791479+abhinandant34@users.noreply.github.com>
2021-10-06 23:51:02 +0530

    hello world

--- src/data.js
+++ src/data.js
@@ -3,5 +3,10 @@ export const resourcesData = [
       courseName: "Aarhan Khan",
       brief: "developer of this site",
       link: "https://www.instagram.com/aarhan_._/",
-    },  
+    },
+    {
+      courseName: "x",
+      brief: "hello world",
+      link: "https://www.tensorflow.org/",
+    },    
   ];
