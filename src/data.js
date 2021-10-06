8026a7348d19e5905fbd2490fea0d799e1b82a5e
Aarhan Khan <aarhankhan212@gmail.com>
2021-10-06 21:16:32 +0530

    updated readme.md

--- src/data.js
+++ src/data.js
@@ -3,42 +3,5 @@ export const resourcesData = [
       courseName: "Aarhan Khan",
       brief: "developer of this site",
       link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },
-    {
-        courseName: "Aarhan Khan",
-        brief: "developer of this site",
-        link: "https://www.instagram.com/aarhan_._/",
-    },   
-
-    
+    },  
   ];
