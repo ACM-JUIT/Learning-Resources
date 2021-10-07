ef0fac6b6f5970d5701b458cbcf372edf0f60b41
deep429 <deepshekhar429@gmail.com>
2021-10-07 09:28:04 +0530

    Android Studio Tutorial

--- src/data.js
+++ src/data.js
@@ -3,5 +3,10 @@ export const resourcesData = [
       courseName: "Aarhan Khan",
       brief: "developer of this site",
       link: "https://www.instagram.com/aarhan_._/",
-    },  
+    }, 
+    {
+      courseName: "Android Studio",
+      brief: "Android studio tutorial for beginner",
+      link: "https://www.youtube.com/playlist?list=PLu0W_9lII9aiL0kysYlfSOUgY5rNlOhUd",
+    },   
   ];
