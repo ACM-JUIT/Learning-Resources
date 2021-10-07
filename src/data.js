e1ea4460a47cc8749a5c95d989acef26b1f1a376
Ujjwal Pathak <pathak2002ujjwal@gmail.com>
2021-10-07 08:40:49 +0530

    added c++ course link

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const resourcesData = [
     {
-      courseName: "Aarhan Khan",
-      brief: "developer of this site",
-      link: "https://www.instagram.com/aarhan_._/",
+      courseName: "C++ by CodeWithHarry",
+      brief: "C++ Basic Programming",
+      link: "https://www.codewithharry.com/videos/cpp-tutorials-in-hindi-1",
     },  
   ];
