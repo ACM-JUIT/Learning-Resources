68f844956b45a4b943af3d633207ef28f6016659
SMZaeem <zaeemdhoniline711@gmail.com>
2021-10-12 16:52:15 +0530

    learning resources

--- src/data.js
+++ src/data.js
@@ -119,4 +119,9 @@ export const resourcesData = [
       brief: "going from 5 to 1 stars in codechef in one month",
       link: "",
     },
+ {
+      courseName: "JAVA",
+      brief: "learn JAVA and become a JAVA developer",
+      link: "youtube.com",
+    },
   ];
